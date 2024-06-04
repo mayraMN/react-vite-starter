113abfc206f855c3192e7ed9ea64aa3b77503dfa
Mikel <lem_bar@hotmail.com>
2024-06-04 19:10:14 +0200

    SearchBar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Header } from './Components/Header/Header'
+import { SearchBar } from './Components/SearchBar/SearchBar'
 import { Card } from './Components/Card/Card'
 
 function App() {
   return (
     <>
       <Header />
+      <SearchBar />
       <Card />
     </>
   )
--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,11 +1,10 @@
 import styles from './Header.module.css'
 
-
 export const Header = () => {
     return (
         <header className={styles.header}>
             <nav className={styles.navbar}>
-                <img src="/common_icons/pokeball.svg" alt="" />
+                <img src="/commonIcons/pokeball.svg" alt="" />
                 <h1>Pokédex</h1>
             </nav>
         </header>
