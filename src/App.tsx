8ca70507eb7747d5b38578af30fcbc9f831a3b43
Mikel <lem_bar@hotmail.com>
2024-06-07 17:36:42 +0200

    getAPI con Promise.all

--- src/App.tsx
+++ src/App.tsx
@@ -4,40 +4,44 @@ import { Card } from './Components/Card/Card'
 import { useEffect, useState } from 'react'
 
 type Stat = {
-  name: "HP" | "ATK" | "DEF" | "SAT" | "SDF" | "SPD";
-  value: number;
+  name: 'HP' | 'ATK' | 'DEF' | 'SAT' | 'SDF' | 'SPD'
+  value: number
 }
 
 type Pokemon = {
-  name: string;
-  id: number;
-  types: string[];
-  height: number;
-  weight: number;
-  stats: Stat[];
-  isVisible: boolean;
+  name: string
+  id: number
+  types: string[]
+  height: number
+  weight: number
+  stats: Stat[]
+  isVisible: boolean
 }
 
 function App() {
   const [pokemons, setPokemons] = useState<Pokemon[]>([])
 
-  async function getApi() {
+  async function getAPI() {
     const response = await fetch('https://pokeapi.co/api/v2/generation/1')
-    const json = await response.json();
-    const pokemons = json.pokemon_species;
+    const json = await response.json()
+    const pokemons = json.pokemon_species
 
-    const names = pokemons.map(async (pokemon) => {
-      const name = pokemon.name;
+    const promises = pokemons.map(async pokemon => {
+      const name = pokemon.name
       const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
-      const json = await response.json();
-      return json.name;
+      const json = await response.json()
+      return json.name
     })
 
-    setPokemons(names)
+    const results = await Promise.all(promises)
+    console.log('RESULTS', results)
+
+    // await console.log(promises)
+    // setPokemons(promises)
   }
 
   useEffect(() => {
-    getApi()
+    getAPI()
   }, [])
 
   return (
